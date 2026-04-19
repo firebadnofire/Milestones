bfc57d6c914e2c920823ce22f05cdea968dcaab4
firebadnofire <william@archuser.org>
2026-04-19 03:19:07 -0400

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "org.archuser.milestones"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.1"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
